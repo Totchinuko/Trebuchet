47c751e5c246eb51d8b8d68db489863a17cc74eb
Totchinuko <contact@totchinuko.fr>
2023-08-03 01:06:56 +0200

    Add False2Hidden

---
+++ GoogGUI/Controls/False2Hidden.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GoogGUI.Controls
+{
+    public class False2Hidden : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool boolean)
+                return boolean ? Visibility.Visible : Visibility.Hidden;
+            return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
