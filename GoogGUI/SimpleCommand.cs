245f430740cb602d86de40c4b79eb075a0daed24
Totchinuko <contact@totchinuko.fr>
2023-08-03 01:08:42 +0200

    Simple ICommand for button click

---
+++ GoogGUI/SimpleCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GoogGUI
+{
+    public class SimpleCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged;
+
+        private readonly Action<object?> _execute;
+
+        public SimpleCommand(Action<object?> execute)
+        {
+            _execute = execute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
