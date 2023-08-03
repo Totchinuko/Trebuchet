0f17a597e25b9c6dd7ddd70b6be6fc901662a663
Totchinuko <contact@totchinuko.fr>
2023-08-03 11:27:52 +0200

    Cleaning tab code out of MainWindow

--- GoogGUI/MainWindow.xaml.cs
+++ GoogGUI/MainWindow.xaml.cs
@@ -9,33 +9,10 @@ namespace GoogGUI
     /// </summary>
     public partial class MainWindow : Window
     {
-        private ICommand _tabCommand;
-        private GButton _currentTab;
         public MainWindow()
         {
             InitializeComponent();
-
-            TabCommand = new SimpleCommand(OnTabClicked);
-
             DataContext = this;
-        }
-
-        public ICommand TabCommand { get => _tabCommand; set => _tabCommand = value; }
-        private void OnTabClicked(object? obj)
-        {
-            if (obj == null || obj is not GButton gbutton)
-                return;
-
-            if (_currentTab != null)
-                _currentTab.ButtonAccent = false;
-            _currentTab = gbutton;
-            if (_currentTab != null)
-                _currentTab.ButtonAccent = true;
-        }
-
-        private void Settings_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            ContentPanelPresenter.Content = new SettingsContent();
         }
     }
 }
