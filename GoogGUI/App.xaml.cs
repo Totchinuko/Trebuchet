50a47132125c86a31b079d8c8bfc0cab369c55ff
Totchinuko <contact@totchinuko.fr>
2023-08-04 17:48:14 +0200

    Adding Exception Modal when catching app exceptions

--- GoogGUI/App.xaml.cs
+++ GoogGUI/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace GoogGUI
 {
@@ -13,5 +15,27 @@ namespace GoogGUI
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(OnUnhandledException);
+            System.Windows.Forms.Application.ThreadException += new ThreadExceptionEventHandler(OnApplicationThreadException);
+            Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(OnDispatcherUnhandledException);
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            new ExceptionModal(e.Exception).ShowDialog();
+        }
+
+        private void OnApplicationThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            new ExceptionModal(e.Exception).ShowDialog();
+        }
+
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            new ExceptionModal(((Exception)e.ExceptionObject)).ShowDialog();
+        }
     }
 }
---
+++ GoogGUI/ExceptionModal.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace GoogGUI
+{
+    public class ExceptionModal : BaseModal
+    {
+        private string _errorMessage = string.Empty;
+        private string _errorTitle = string.Empty;
+        private bool _exitApp = false;
+
+        public ExceptionModal(Exception exception)
+        {
+            CloseCommand = new SimpleCommand(OnCloseModal);
+            _errorTitle = "Internal Exception";
+            _errorMessage = exception.GetAllExceptions();
+            Clipboard.SetText(_errorMessage);
+        }
+
+        public ICommand CloseCommand { get; private set; }
+        public string ErrorMessage { get => _errorMessage; set => _errorMessage = value; }
+        public string ErrorTitle { get => _errorTitle; set => _errorTitle = value; }
+        public override int ModalHeight => 400;
+
+        public override string ModalTitle => "Exception";
+
+        public override DataTemplate Template => (DataTemplate)Application.Current.Resources["ExceptionModal"];
+
+        public override int ModalWidth => 650;
+
+        public override void OnWindowClose()
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OnCloseModal(object? obj)
+        {
+            _windows?.Close();
+            Application.Current.Shutdown();
+        }
+    }
+}
