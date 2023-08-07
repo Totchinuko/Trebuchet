ba378218317a9f8726ea9330195f42941a9c5aef
Totchinuko <contact@totchinuko.fr>
2023-08-07 22:43:59 +0200

    Helper function for finding stuff

--- GoogGUI/GuiExtensions.cs
+++ GoogGUI/GuiExtensions.cs
@@ -169,5 +169,17 @@ namespace GoogGUI
                 parent.SetValue(property, value);
             }
         }
+
+        public static IEnumerable<T> FindVisualChildren<T>(DependencyObject depObj) where T : DependencyObject
+        {
+            if (depObj == null) yield return (T)Enumerable.Empty<T>();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject ithChild = VisualTreeHelper.GetChild(depObj, i);
+                if (ithChild == null) continue;
+                if (ithChild is T t) yield return t;
+                foreach (T childOfChild in FindVisualChildren<T>(ithChild)) yield return childOfChild;
+            }
+        }
     }
 }
