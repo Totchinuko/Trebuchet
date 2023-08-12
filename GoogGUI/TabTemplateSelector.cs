a27a5998c75ebeb2cec973bcd58ae292fa423efa
Totchinuko <contact@totchinuko.fr>
2023-08-12 15:34:21 +0200

    Prepare tab menu for multiply types like labels

---
+++ GoogGUI/TabTemplateSelector.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GoogGUI
+{
+    public class TabTemplateSelector : DataTemplateSelector
+    {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is Panel)
+                return (DataTemplate)Application.Current.Resources["TabButtonTemplate"];
+            else
+                return (DataTemplate)Application.Current.Resources["TabLabel"];
+        }
+    }
+}
