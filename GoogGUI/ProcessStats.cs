577a6a7e0f419a8851d86960d2d1eca02b41a237
Totchinuko <contact@totchinuko.fr>
2023-08-13 02:05:17 +0200

    Multithreading improvement for stats

--- GoogGUI/ProcessStats.cs
+++ GoogGUI/ProcessStats.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Threading;
@@ -22,20 +23,24 @@ namespace GoogGUI
         private PerformanceCounter? _memoryConsumptionCounter;
         private long _peakMemoryConsumption = 0;
         private DateTime _start;
-        private string _processName;
 
         private DispatcherTimer _timer;
+        private CancellationTokenSource? _source;
+
+        private object theLock = new object();
 
         public ProcessStats()
         {
             _timer = new DispatcherTimer(TimeSpan.FromSeconds(1), DispatcherPriority.Background, OnTick, Application.Current.Dispatcher);
+            CpuUsage = string.Empty;
+            MemoryConsumption = string.Empty;
         }
 
         public event PropertyChangedEventHandler? PropertyChanged;
 
-        public string CpuUsage => string.Format(CPUFormat, _cpuUsage.ToString());
+        public string CpuUsage { get; private set; }
 
-        public string MemoryConsumption => string.Format(MemoryFormat, (_memoryConsumption / 1024 / 1024), (_peakMemoryConsumption / 1024 / 1024));
+        public string MemoryConsumption { get; private set; }
 
         public string Uptime => _process == null ? string.Empty : (DateTime.UtcNow - _start).ToString("c");
 
@@ -48,37 +53,20 @@ namespace GoogGUI
             _process = process;
             _process.EnableRaisingEvents = true;
             _process.Exited += OnProcessExited;
-            _processName = processName;
-
-            Task.Run(() => _memoryConsumptionCounter = new PerformanceCounter("Process V2", "Working Set", _processName + ":" + process.Id)).ContinueWith(OnMemoryCounterCreated);
-            Task.Run(() => _cpuUsageCounter = new PerformanceCounter("Process V2", "% Processor Time", _processName + ":" + process.Id)).ContinueWith(OnCPUCounterCreated);
 
             _start = DateTime.UtcNow;
+
+            _source = new CancellationTokenSource();
+            Task.Run(() => RunCounters(process.Id, processName, _source.Token));
 
             _timer.Start();
             OnPropertyChanged("Running");
         }
 
-        private void OnCPUCounterCreated(Task<PerformanceCounter> task)
-        {
-            _cpuUsageCounter = task.Result;
-        }
-
-        private void OnMemoryCounterCreated(Task<PerformanceCounter> task)
-        {
-            _memoryConsumptionCounter = task.Result;
-        }
-
         protected virtual void OnProcessExited(object? sender, EventArgs e)
         {
             _process = null;
-            _memoryConsumptionCounter?.Dispose();
-            _cpuUsageCounter?.Dispose();
-            _memoryConsumptionCounter = null;
-            _cpuUsageCounter = null;
-
-            _memoryConsumption = _peakMemoryConsumption = 0;
-            _cpuUsage = 0;
+            _source?.Cancel();
             _timer.Stop();
             OnPropertyChanged("Running");
         }
@@ -92,14 +80,42 @@ namespace GoogGUI
         {
             if (_process == null) return;
 
-            _memoryConsumption = (long)(_memoryConsumptionCounter?.NextValue() ?? 0f);
-            _peakMemoryConsumption = Math.Max(_memoryConsumption, _peakMemoryConsumption);
-
-            _cpuUsage = (int)(_cpuUsageCounter?.NextValue() / Environment.ProcessorCount ?? 0f);
+            lock(theLock)
+            {
+                _peakMemoryConsumption = Math.Max(_memoryConsumption, _peakMemoryConsumption);
+                CpuUsage = string.Format(CPUFormat, _cpuUsage.ToString());
+                MemoryConsumption = string.Format(MemoryFormat, (_memoryConsumption / 1024 / 1024), (_peakMemoryConsumption / 1024 / 1024));
+            }
 
             OnPropertyChanged("MemoryConsumption");
             OnPropertyChanged("PeakMemoryConsumption");
             OnPropertyChanged("CpuUsage");
+            OnPropertyChanged("Uptime");
+        }
+
+        protected virtual async Task RunCounters(int processID, string processName, CancellationToken token)
+        {
+            _memoryConsumptionCounter = await Task.Run(() => new PerformanceCounter("Process V2", "Working Set", processName + ":" + processID));
+            _cpuUsageCounter = await Task.Run(() => new PerformanceCounter("Process V2", "% Processor Time", processName + ":" + processID));
+
+            while(!token.IsCancellationRequested)
+            {
+                long memory = (long)(_memoryConsumptionCounter?.NextValue() ?? 0f);
+                int cpu = (int)(_cpuUsageCounter?.NextValue() / Environment.ProcessorCount ?? 0f);
+
+                lock (theLock)
+                {
+                    _memoryConsumption = memory;
+                    _cpuUsage = cpu;
+                }
+
+                await Task.Delay(1000);
+            }
+
+            _memoryConsumptionCounter?.Dispose();
+            _cpuUsageCounter?.Dispose();
+            _memoryConsumptionCounter = null;
+            _cpuUsageCounter = null;
         }
     }
 }
