a79fee72b2f87a4f14d6b9956a3c56968e0b5ee3
Totchinuko <contact@totchinuko.fr>
2023-08-18 02:02:41 +0200

    Adding new empty rcon panel

---
+++ GoogGUI/RconPanel.cs
@@ -0,0 +1,31 @@
+﻿using Goog;
+using System.IO;
+
+namespace GoogGUI
+{
+    [Panel("Rcon", "/Icons/Steam.png", false, 300, "RconPanel", "Server")]
+    public class RconPanel : Panel
+    {
+        public RconPanel(Config config, UIConfig uiConfig) : base(config, uiConfig)
+        {
+            LoadPanel();
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _config.IsInstallPathValid &&
+                   File.Exists(Path.Combine(_config.InstallPath, Config.FolderSteam, Config.FileSteamCMDBin)) &&
+                   _config.ServerInstanceCount > 0;
+        }
+
+        public override void RefreshPanel()
+        {
+            OnCanExecuteChanged();
+            LoadPanel();
+        }
+
+        private void LoadPanel()
+        {
+        }
+    }
+}
