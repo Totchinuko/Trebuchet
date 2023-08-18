6b469f2e8f2339d1b645fa4f7ae377e68e205079
Totchinuko <contact@totchinuko.fr>
2023-08-18 11:57:26 +0200

    Documentation for config/profile files

--- GoogLib/ProfileFile.cs
+++ GoogLib/ProfileFile.cs
@@ -6,18 +6,46 @@ namespace GoogLib
     {
         protected Config Config { get; set; } = default!;
 
+        /// <summary>
+        /// Create a new profile file in memory. Use SaveFile to write it down.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static T CreateProfile(Config config, string path)
         {
-            var profile = CreateFile(path);
-            profile.Config = config;
-            return profile;
+            try
+            {
+                var profile = CreateFile(path);
+                profile.Config = config;
+                return profile;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Failed to create profile file.", ex);
+            }
         }
 
+        /// <summary>
+        /// Load a profile file from disk or create a new one if it doesn't exist.
+        /// </summary>
+        /// <param name="config"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static T LoadProfile(Config config, string path)
         {
-            var profile = LoadFile(path);
-            profile.Config = config;
-            return profile;
+            try
+            {
+                var profile = LoadFile(path);
+                profile.Config = config;
+                return profile;
+            }
+            catch(Exception ex)
+            {
+                throw new Exception("Failed to load profile file.", ex);
+            }
         }
     }
 }
