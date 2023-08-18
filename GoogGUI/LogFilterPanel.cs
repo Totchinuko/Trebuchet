0e024b81f0bc6a34478b295b05a0a273e8ecd404
Totchinuko <contact@totchinuko.fr>
2023-08-18 02:17:06 +0200

    Adding futur log filter panel

---
+++ GoogGUI/LogFilterPanel.cs
@@ -0,0 +1,31 @@
+﻿using Goog;
+using System.IO;
+
+namespace GoogGUI
+{
+    [Panel("Log Filter", "/Icons/Filter.png", false, 400, "LogFilterPanel", "Server")]
+    public class LogFilterPanel : Panel
+    {
+        public LogFilterPanel(Config config, UIConfig uiConfig) : base(config, uiConfig)
+        {
+            LoadPanel();
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return _config.IsInstallPathValid &&
+                   File.Exists(Path.Combine(_config.InstallPath, Config.FolderSteam, Config.FileSteamCMDBin)) &&
+                   _config.ServerInstanceCount > 0;
+        }
+
+        public override void RefreshPanel()
+        {
+            OnCanExecuteChanged();
+            LoadPanel();
+        }
+
+        private void LoadPanel()
+        {
+        }
+    }
+}
