2bb768e4a04a4604b5c2cf772994d630ade7d751
Totchinuko <contact@totchinuko.fr>
2023-08-20 04:18:42 +0200

    Adding download settings to the panel

--- GoogLib/Config.cs
+++ GoogLib/Config.cs
@@ -66,7 +66,7 @@ namespace Goog
 
         public int ServerInstanceCount { get; set; } = 0;
 
-        public bool VerifyAll { get; set; } = false;
+        public bool VerifyAll { get; set; } = true;
 
         public string VersionFolder => IsTestLive ? FolderTestLive : FolderLive;
 
