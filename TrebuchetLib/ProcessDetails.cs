1307ea1060836c3b32e2dc1b54899ecdef6a1095
Totchinuko <contact@totchinuko.fr>
2023-08-25 13:18:04 +0200

    Adding new process data details for a rework of the process event chain

---
+++ TrebuchetLib/ProcessDetails.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Trebuchet;
+
+namespace TrebuchetLib
+{
+    public class ProcessDetails
+    {
+        public ProcessDetails(ProcessDetails details, ProcessData data, ProcessState state)
+        {
+            Arguments = data.args;
+            Filename = data.filename;
+            PID = (uint)data.pid;
+            ProcessName = data.processName;
+            State = state;
+            WorkingDirectory = Path.GetDirectoryName(Filename) ?? string.Empty;
+            Modlist = details.Modlist;
+            Profile = details.Profile;
+        }
+
+        public ProcessDetails(string profile, string modlist)
+        {
+            Profile = profile;
+            Modlist = modlist;
+            Arguments = string.Empty;
+            Filename = string.Empty;
+            PID = 0;
+            ProcessName = string.Empty;
+            State = ProcessState.NEW;
+            WorkingDirectory = string.Empty;
+        }
+
+        public ProcessDetails(string profile, string modlist, ProcessData data, ProcessState state)
+        {
+            Profile = profile;
+            Modlist = modlist;
+            State = state;
+            Arguments = data.args;
+            Filename = data.filename;
+            PID = (uint)data.pid;
+            ProcessName = data.processName;
+            WorkingDirectory = Path.GetDirectoryName(Filename) ?? string.Empty;
+        }
+
+        public ProcessDetails(ProcessDetails details, ProcessState state)
+        {
+            Arguments = details.Arguments;
+            Filename = details.Filename;
+            PID = details.PID;
+            ProcessName = details.ProcessName;
+            State = state;
+            WorkingDirectory = details.WorkingDirectory;
+            Profile = details.Profile;
+            Modlist = details.Modlist;
+        }
+
+        public ProcessDetails()
+        {
+            Arguments = string.Empty;
+            Filename = string.Empty;
+            PID = 0;
+            ProcessName = string.Empty;
+            State = ProcessState.NEW;
+            WorkingDirectory = string.Empty;
+            Profile = string.Empty;
+            Modlist = string.Empty;
+        }
+
+        public string Arguments { get; }
+
+        public string Filename { get; }
+
+        public string Modlist { get; }
+
+        public uint PID { get; }
+
+        public string ProcessName { get; }
+
+        public string Profile { get; }
+
+        public ProcessState State { get; }
+
+        public string WorkingDirectory { get; }
+    }
+}
---
+++ TrebuchetLib/ProcessState.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrebuchetLib
+{
+    public enum ProcessState
+    {
+        NEW,
+        FAILED,
+        RUNNING,
+        ONLINE,
+        CRASHED,
+        STOPPING,
+        STOPPED,
+    }
+}
