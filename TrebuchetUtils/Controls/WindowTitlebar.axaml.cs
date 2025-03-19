d969d8366bd761fd06232cc3197452f26a6c16f1
Totchinuko <contact@totchinuko.fr>
2025-03-19 03:54:20 +0100

    Replace svg and png with font icons

--- TrebuchetUtils/Controls/WindowTitlebar.axaml.cs
+++ TrebuchetUtils/Controls/WindowTitlebar.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Interactivity;
 using Avalonia.Media;
 
@@ -11,7 +12,6 @@ namespace TrebuchetUtils.Controls
     /// </summary>
     public partial class WindowTitlebar : UserControl
     {
-        public static readonly StyledProperty<IImage?> CloseIconProperty = AvaloniaProperty.Register<WindowTitlebar, IImage?>(nameof(CloseIcon));
 
         public static readonly StyledProperty<bool> DisableCloseProperty = AvaloniaProperty.Register<WindowTitlebar, bool>(nameof(DisableClose));
 
@@ -23,35 +23,39 @@ namespace TrebuchetUtils.Controls
 
         public static readonly StyledProperty<IImage?> LogoIconProperty = AvaloniaProperty.Register<WindowTitlebar, IImage?>(nameof(LogoIcon));
 
-        public static readonly StyledProperty<IImage?> MaximizeIconProperty = AvaloniaProperty.Register<WindowTitlebar, IImage?>(nameof(MaximizeIcon));
-
-        public static readonly StyledProperty<IImage?> MinimizeIconProperty = AvaloniaProperty.Register<WindowTitlebar, IImage?>(nameof(MinimizeIcon));
-
-        public static readonly StyledProperty<IImage?> RestoreIconProperty = AvaloniaProperty.Register<WindowTitlebar, IImage?>(nameof(RestoreIcon));
-
         public static readonly StyledProperty<string> TitleProperty = AvaloniaProperty.Register<WindowTitlebar, string>(nameof(Title));
-
-        private WindowState _state;
+        
+        public static readonly StyledProperty<bool> IsMaximizedProperty = AvaloniaProperty.Register<WindowTitlebar, bool>(nameof(IsMaximized));
 
         public WindowTitlebar()
         {
-            CloseIconProperty.Changed.AddClassHandler<WindowTitlebar>(OnCloseIconChanged);
             DisableCloseProperty.Changed.AddClassHandler<WindowTitlebar>(OnDisableChanged);
             DisableMaximizeProperty.Changed.AddClassHandler<WindowTitlebar>(OnDisableChanged);
             DisableMinimizeProperty.Changed.AddClassHandler<WindowTitlebar>(OnDisableChanged);
             LogoIconProperty.Changed.AddClassHandler<WindowTitlebar>(OnLogoIconChanged);
-            MaximizeIconProperty.Changed.AddClassHandler<WindowTitlebar>(OnMaximizeIconChanged);
-            MinimizeIconProperty.Changed.AddClassHandler<WindowTitlebar>(OnMinimizeIconChanged);
-            RestoreIconProperty.Changed.AddClassHandler<WindowTitlebar>(OnRestoreIconChanged);
             TitleProperty.Changed.AddClassHandler<WindowTitlebar>(OnTitleChanged);
             InitializeComponent();
+
         }
 
-        public IImage? CloseIcon
+        protected override void OnLoaded(RoutedEventArgs e)
         {
-            get => GetValue(CloseIconProperty);
-            set => SetValue(CloseIconProperty, value);
+            base.OnLoaded(e);
+            if (TopLevel.GetTopLevel(this) is not Window window)
+                throw new ApplicationException("Title bar only function on desktop lifetime");
+            window.PropertyChanged += OnWindowPropertyChanged;
+            IsMaximized = window.WindowState == WindowState.Maximized;
         }
+
+        private void OnWindowPropertyChanged(object? sender, AvaloniaPropertyChangedEventArgs e)
+        {
+            if (e.Property.Name == "WindowState")
+            {
+                if (TopLevel.GetTopLevel(this) is not Window window) return;
+                IsMaximized = window.WindowState == WindowState.Maximized;
+            }
+        }
+
 
         public bool DisableClose
         {
@@ -83,24 +87,6 @@ namespace TrebuchetUtils.Controls
             set => SetValue(LogoIconProperty, value);
         }
 
-        public IImage? MaximizeIcon
-        {
-            get => GetValue(MaximizeIconProperty);
-            set => SetValue(MaximizeIconProperty, value);
-        }
-
-        public IImage? MinimizeIcon
-        {
-            get => GetValue(MinimizeIconProperty);
-            set => SetValue(MinimizeIconProperty, value);
-        }
-
-        public IImage? RestoreIcon
-        {
-            get => GetValue(RestoreIconProperty);
-            set => SetValue(RestoreIconProperty, value);
-        }
-
         public string Title
         {
             get => GetValue(TitleProperty);
@@ -109,11 +95,12 @@ namespace TrebuchetUtils.Controls
 
         public bool DisplayWindowControls => !Utils.UseOsChrome();
 
-        private static void OnCloseIconChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
+        public bool IsMaximized
         {
-            sender.CloseImage.Source = sender.CloseIcon;
+            get => GetValue(IsMaximizedProperty);
+            set => SetValue(IsMaximizedProperty, value);
         }
-
+        
         private static void OnDisableChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
         {
             sender.CloseBtn.IsEnabled = !sender.GetValue(DisableCloseProperty);
@@ -124,23 +111,6 @@ namespace TrebuchetUtils.Controls
         private static void OnLogoIconChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
         {
             sender.AppLogo.Source = sender.LogoIcon;
-        }
-
-        private static void OnMaximizeIconChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
-        {
-            if (sender._state == WindowState.Normal)
-                sender.MaximizeImage.Source = sender.MaximizeIcon;
-        }
-
-        private static void OnMinimizeIconChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
-        {
-            sender.MinimizeImage.Source = sender.MinimizeIcon;
-        }
-
-        private static void OnRestoreIconChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
-        {
-            if (sender._state == WindowState.Maximized)
-                sender.MaximizeImage.Source = sender.RestoreIcon;
         }
 
         private static void OnTitleChanged(WindowTitlebar sender, AvaloniaPropertyChangedEventArgs e)
@@ -161,14 +131,10 @@ namespace TrebuchetUtils.Controls
             if (window.WindowState == WindowState.Normal)
             {
                 window.WindowState = WindowState.Maximized;
-                _state = WindowState.Maximized;
-                MaximizeImage.Source = RestoreIcon;
             }
             else if (window.WindowState == WindowState.Maximized)
             {
                 window.WindowState = WindowState.Normal;
-                _state = WindowState.Normal;
-                MaximizeImage.Source = MaximizeIcon;
             }
         }
 
@@ -177,7 +143,6 @@ namespace TrebuchetUtils.Controls
             if (TopLevel.GetTopLevel(this) is not Window window) return;
 
             window.WindowState = WindowState.Minimized;
-            _state = WindowState.Minimized;
         }
     }
 }
