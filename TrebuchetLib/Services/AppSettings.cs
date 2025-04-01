c269fe1f4b44aea8f7f6c88f038e28e13e06152f
Totchinuko <contact@totchinuko.fr>
2025-04-01 18:18:48 +0200

    Fix broken api key

--- TrebuchetLib/Services/AppSettings.cs
+++ TrebuchetLib/Services/AppSettings.cs
@@ -1,6 +1,9 @@
+using System.Text.Json.Serialization;
+
 namespace TrebuchetLib.Services;
 
 public class AppSettings
 {
+    [JsonPropertyName("apikey")]
     public string ApiKey { get; set; } = string.Empty;
 }
